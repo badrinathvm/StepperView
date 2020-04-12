df77b9ca3a6752f11147dd62125d4b03b3f84c3c
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-04-11 19:23:56 -0700

    Example refactoring

--- Sources/StepperView/Extension/View+Extensions.swift
+++ Sources/StepperView/Extension/View+Extensions.swift
@@ -80,7 +80,6 @@ public extension View {
     func getYPosition(for alignment: StepperAlignment) -> CGFloat {
         switch alignment {
         case .top:
-             print("Top Value is \(VerticalAlignment.topValue)")
             return VerticalAlignment.topValue
         case .center:
             return VerticalAlignment.centerValue
