ded8055582beef61fcb87d98b64fdb403352b3e1
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-04-14 12:45:39 -0700

    Added a unit test for StepperAlignment

--- Tests/StepperViewTests/StepperViewTests.swift
+++ Tests/StepperViewTests/StepperViewTests.swift
@@ -1,15 +1,16 @@
 import XCTest
 @testable import StepperView
 
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 final class StepperViewTests: XCTestCase {
-    func testExample() {
+    func testStepAlignment() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertTrue(true)
+        XCTAssertTrue(StepperAlignment.allCases.count == 3)
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testExample", testStepAlignment)
     ]
 }
