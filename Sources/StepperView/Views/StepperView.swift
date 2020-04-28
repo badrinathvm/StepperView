67517b41a8ac511e9c5dc95e9dcecfe522efb985
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-04-27 17:44:16 -0700

    Fixed conflicts in gitignore and updated jazzy documentation for stepper view and enums

--- Sources/StepperView/Views/StepperView.swift
+++ Sources/StepperView/Views/StepperView.swift
@@ -7,42 +7,22 @@
 
 import SwiftUI
 
-// MARK: - Stepper View alignments
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-public enum StepperAlignment: String, CaseIterable {
-    case top = "Top", center = "Center", bottom = "Bottom"
-}
-
-// MARK: - Stepper Indication options
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-public enum StepperIndicationType<Content:View> {
-    public typealias Width = CGFloat
-    case circle(Color, Width)
-    case image(Image, Width)
-    case custom(Content)
-}
-
-// MARK: - Line Options for Step Customization
-@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
-public enum StepperLineOptions {
-    case defaults
-    case custom(CGFloat, Color)
-}
-
-// MARK: - Indicates the types of Setp Indicator mode
-public enum StepperMode:String, CaseIterable {
-    case vertical
-    case horizontal
-}
-
-// MARK: - Stepper View Implementation
+/// A View for Step Indications.
+///
+/// You create step indicator view either  in `vertical` or `horizontal` mode
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct StepperView: View {
+    /// contains list of steps to be rendered next to Indicators
     @Environment(\.steps) var steps
+    /// alignments to place the step indicators
     @Environment(\.alignments) var alignments
+    /// step indicator type can be a `Circle` , `Image` or `Custom`
     @Environment(\.indicationType) var indicationType
+    /// to customise the `width ` , `Color` of the line
     @Environment(\.lineOptions) var lineOptions
+    /// aligns the step indicator either in `vertical` or `horizontal`
     @Environment(\.stepperMode) var stepperMode
+    /// spacing between each of the step indicators
     @Environment(\.spacing) var verticalSpacing
     
     public init() { }
@@ -65,4 +45,72 @@ public struct StepperView: View {
                 .eraseToAnyView()
         }
     }
+}
+
+
+// MARK: - Stepper View alignments
+/**
+    Options for stepper view alignments
+ 
+    ````
+    case top
+    case center
+    case bottoom
+    ````
+*/
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public enum StepperAlignment: String, CaseIterable {
+    /// aligns step Indicator  to top
+    case top = "Top"
+    /// aligns step Indicator  to center
+    case center = "Center"
+    /// aligns step Indicator  to bottom
+    case bottom = "Bottom"
+}
+
+// MARK: - Stepper Indication options
+/**
+    Options for displaying step indications can be either `Circle` or `Image`or `custom(View)`
+ 
+    ````
+    case circle(Color, Width)op
+    case image(Image, Width)
+    case custom(Content)
+    ````
+ */
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public enum StepperIndicationType<Content:View> {
+    public typealias Width = CGFloat
+    case circle(Color, Width)
+    case image(Image, Width)
+    case custom(Content)
+}
+
+// MARK: - Line Options for Step Customization
+/**
+   Options for customizing line with either 1defaults` or  custom `Width` and `Color`
+
+   ````
+   case defaults
+   case custom(CGFloat, Color)
+   ````
+*/
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public enum StepperLineOptions {
+    case defaults
+    case custom(CGFloat, Color)
+}
+
+// MARK: - Indicates the types of Setp Indicator mode
+/**
+   Options for aligns the step indicator either in `vertical` or `horizontal`
+
+   ````
+   case vertical
+   case horizontal
+   ````
+*/
+public enum StepperMode:String, CaseIterable {
+    case vertical
+    case horizontal
 }
