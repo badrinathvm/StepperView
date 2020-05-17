1b7e36dd7cb47774a533af306d1b78aa92503851
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2020-05-17 13:55:38 -0700

    Github workflow example

---
+++ Example/StepperView/ExampleView8.swift
@@ -0,0 +1,67 @@
+//
+//  ExampleView8.swift
+//  StepperView_Example
+//
+//  Created by Venkatnarayansetty, Badarinath on 5/17/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import StepperView
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+struct ExampleView8: View {
+    
+    let indicators = [
+           StepperIndicationType.custom(CircledIconView(image: Image("tasks"),
+                                                        width: 30,
+                                                        strokeColor: Color.black)
+                               .eraseToAnyView()),
+           StepperIndicationType.custom(CircledIconView(image: Image("branch"),
+                                                        width: 30,
+                                                        strokeColor: Color.black)
+                               .eraseToAnyView()),
+           StepperIndicationType.custom(CircledIconView(image: Image("commit"),
+                                                        width: 30,
+                                                        strokeColor: Color.black)
+                               .eraseToAnyView()),
+           StepperIndicationType.custom(CircledIconView(image: Image("codereview"),
+                                                           width: 30,
+                                                           strokeColor: Color.black)
+                                  .eraseToAnyView()),
+           StepperIndicationType.custom(CircledIconView(image: Image("merge"),
+                                                           width: 30,
+                                                           strokeColor: Color.black)
+                                  .eraseToAnyView())]
+    
+    let steps = [TextView(text:"Manage Tasks", font: .system(size: 14, weight: .semibold)),
+                 TextView(text:"Branch", font: .system(size: 14, weight: .semibold)),
+                 TextView(text:"Commit", font: .system(size: 14, weight: .semibold)),
+                 TextView(text:"Code review", font: .system(size: 14, weight: .semibold)),
+                 TextView(text:"Merge and release", font: .system(size: 14, weight: .semibold))]
+    
+    let pitStops = [  PitStopStep(view: TextView(text:GithubPitstops.p1).eraseToAnyView(), lineOptions: .custom(1, Color.black)),
+                      PitStopStep(view: TextView(text:GithubPitstops.p2).eraseToAnyView(), lineOptions: .custom(1, Color.black)),
+                      PitStopStep(view: TextView(text:GithubPitstops.p3).eraseToAnyView(), lineOptions: .custom(1, Color.black)),
+                      PitStopStep(view: TextView(text:GithubPitstops.p4).eraseToAnyView(), lineOptions: .custom(1, Color.black)),
+                      PitStopStep(view: TextView(text:GithubPitstops.p5).eraseToAnyView(), lineOptions: .custom(1, Color.black))
+                    ]
+    
+    var body: some View {
+        StepperView()
+                .addSteps(steps)
+                .indicators(indicators)
+                .addPitStops(pitStops)
+                .spacing(100)
+               .padding(.top, -30)
+    }
+}
+
+struct GithubPitstops {
+    static var p1 = "\n\nTriage Notifications\nBrowse Repos\nCreate an issue\nComment on issues\nAssign self to issue"
+    static var p2 = "Fork or Clone repo\ngit checkout -b branch\ngit stash"
+    static var p3 = "git add -p\ngit diff .\ngit commit -m\ngit push origin branch"
+    static var p4 = "Open pull request\ngit checkout pr-branch\nReview and comment"
+    static var p5 = "View checks\ngit rebase\ngit merge\ngit tag"
+}
