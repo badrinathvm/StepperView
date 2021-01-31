7229ec0fc5cb1bd632d179ada0ef331c23557261
Badarinath Venkatnarayansetty <badrinathvm@gmail.com>
2021-01-31 10:08:28 -0800

    SCroll bar example (#67)
    Co-authored-by: Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>

--- Example/StepperView/ExampleView7.swift
+++ Example/StepperView/ExampleView7.swift
@@ -17,21 +17,29 @@ struct ExampleView7: View {
                                                 .animation(NumberedCircleView(text: "2")),
                                                 .animation(NumberedCircleView(text: "3")),
                                                 .animation(NumberedCircleView(text: "4")),
-                                                .animation(NumberedCircleView(text: "5"))]
+                                                .animation(NumberedCircleView(text: "5")),
+        .animation(NumberedCircleView(text: "6"))
+    ]
     
     let set = [ Text("Campaign").font(.caption),
                      Text("Contents").font(.caption),
                      Text("Recipients").font(.caption),
                      Text("Delivery").font(.caption),
-                     Text("Tracking").font(.caption)]
+                     Text("Tracking").font(.caption),
+                     Text("Complete").font(.caption)
+    ]
     
     var body: some View {
-        StepperView()
-            .addSteps(self.set)
-            .indicators(self.indicationTypes)
-            .stepIndicatorMode(StepperMode.horizontal)
-            .spacing(50)
-            .lineOptions(StepperLineOptions.custom(1, Colors.blue(.teal).rawValue))
-            .loadingAnimationTime(0.02) //speed of the animation
+        ScrollView(Axis.Set.horizontal, showsIndicators: false) {
+            StepperView()
+                .addSteps(self.set)
+                .indicators(self.indicationTypes)
+                .stepIndicatorMode(StepperMode.horizontal)
+                .spacing(50)
+                .lineOptions(StepperLineOptions.custom(1, Colors.blue(.teal).rawValue))
+                .loadingAnimationTime(0.02) //speed of the animation
+                .padding(.horizontal, 40)
+                .frame(height: 300)
+        }
     }
 }
